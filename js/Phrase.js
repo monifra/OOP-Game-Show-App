145c39157e4d2d6124bb807fa0dbf1a28f5b3757
monifra <monia.frankowska@gmail.com>
2019-12-23 16:35:07 +0100

    add addPhraseToDisplay()

--- js/Phrase.js
+++ js/Phrase.js
@@ -6,7 +6,26 @@ class Phrase{
     constructor(phrase){
         this.phrase = phrase.toLowerCase();
     }
+    /**
+    * Display phrase on game board
+    */
     addPhraseToDisplay(){
-        
+        //select parent div
+        const parentDiv = document.querySelector('#phrase');
+        const ul = document.querySelector('ul');
+        const splitPhrase = this.phrase.split(''); //splits phrase into an array of characters
+        console.log(splitPhrase);
+        //loops trough an array of letters and append them to the page
+        splitPhrase.forEach(letter =>{
+            const li = document.createElement('li');
+            li.textContent = letter;
+            if(letter !== ""){
+                li.className = 'letter';
+            }else if(letter === ""){
+                li.className = 'space';
+            }
+            ul.appendChild(li);
+        });
+        console.log(parentDiv);
     }
 }
